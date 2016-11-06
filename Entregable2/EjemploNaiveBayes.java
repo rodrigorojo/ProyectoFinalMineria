1020be7c99ca5baa1221a1dd1bb94e176c7e20fb
JuanCarlos <juancarloslopez0607@gmail.com>
2016-11-05 21:00:53 -0600

    ya clasifica bien con el ejemplo

---
+++ Entregable2/EjemploNaiveBayes.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class EjemploNaiveBayes {
+  public static void main(String[] args) {
+      String[] clases = {"Platano","Naranja","Otros"};
+      String[] atributos = {"Alargada","No Alargada","Dulce","No Dulce","Amarilla","No Amarilla"};
+      double[][] frecuencias = {
+        {400,100,350,150,450,50,500},
+        {0,300,150,150,300,0,300},
+        {100,100,150,50,50,150,200},
+        {500,500,650,350,800,200,1000}
+      };
+      NaiveBayes frutas = new NaiveBayes(clases,atributos,frecuencias);
+      String[] predice = {"Alargada","Dulce","Amarilla"};
+      String prediccion = frutas.clasifica(predice);
+      //frutas.imprimetabla();
+      System.out.println("con los atributos:");
+      for (String pr : predice) {
+        System.out.println(pr);
+      }
+      System.out.println("cae en la clase: "+prediccion);
+  }
+}
--- Entregable2/NaiveBayes.java
+++ Entregable2/NaiveBayes.java
@@ -4,22 +4,23 @@ public class NaiveBayes {
 
 	String[] clases;
 	String[] atributos;
-	int[][] tabla;
+	double[][] tabla;
 	int tc, ta;
 
-	public NaiveBayes(String[] c, String[] a, int[][] t) {
+	public NaiveBayes(String[] c, String[] a, double[][] t) {
 		this.clases = c;
 		this.atributos = a;
 		this.tabla = t;
-		tc = c.length-1;
-		ta = a.length-1;
+		tc = (int)c.length;
+		ta = (int)a.length;
 	}
 
 	public String clasifica(String[] variables) {
-		String clasepred = "";
+		String clasepred = "s";
 		double max = -1;
 		for (String cs : this.clases) {
 			double pvp = prob_var_pred(variables,cs);
+			//System.out.println("varpres:"+pvp);
 			if (pvp > max) {
 				max = pvp;
 				clasepred = cs;
@@ -48,6 +49,7 @@ public class NaiveBayes {
 		for (String vs : variables) {
 			int v = Arrays.asList(this.atributos).indexOf(vs);
 			double pv = tabla[tc][v]/tabla[tc][ta];
+			//System.out.println("predcadaz"+tabla[tc][v]+"/"+tabla[tc][ta] +"="+pv);
 			probac *= pv;
 		}
 		return probac;
@@ -56,13 +58,29 @@ public class NaiveBayes {
 	public double prob_var_pred(String[] variables, String clase) {
 		double probac = 1.0;
 		double pc = probclase(clase);
+		//System.out.println("prob clase:"+pc);
 		probac *= pc;
 		for (String vs : variables) {
 			double pv = bayes(vs,clase);
 			probac *= pv;
 		}
 		double pz = probz(variables);
+		//System.out.println("prob z:"+pz);
 		probac *= (1/pz);
+		//System.out.println("prob vars predictivas:"+probac);
 		return probac;
 	}
+
+	/*public void imprimetabla() {
+		for (int k = 0; k < ta+1;k++) {
+			System.out.print("|"+atributos[k]);
+		}
+		System.out.println("");
+		for (int i = 0; i < tc+1;i++) {
+			for(int j = 0;j<ta+1;j++) {
+				System.out.print("|"+tabla[i][j]);
+			}
+			System.out.println("|"+clases[i]);
+		}
+	}*/
 }
